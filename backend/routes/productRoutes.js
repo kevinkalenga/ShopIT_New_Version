ebda531f1fb1cc5eb96cd4e20dd75c16f5c2d45d
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-09 13:23:30 +0200

    Adding user in products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,11 @@ import { isAuthenticatedUser,  authorizeRoles } from '../middlewares/auth.js';
 import { deleteProduct, getProductDetails, getProducts, newProduct, updateProduct } from '../controllers/productControllers.js';
 const router = express.Router()
 
-router.route("/products").get(isAuthenticatedUser, authorizeRoles('admin') , getProducts);
-router.route("/admin/products").post(newProduct);
+router.route("/products").get(getProducts);
+router.route("/admin/products").post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 
 router.route("/products/:id").get(getProductDetails);
-router.route("/products/:id").put(updateProduct);
-router.route("/products/:id").delete(deleteProduct);
+router.route("/products/:id").put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
+router.route("/products/:id").delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
 
 export default router;
