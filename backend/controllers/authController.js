d8b1a0a224dffa089a8ad4d65b6d3b1bd4198753
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-10 09:53:24 +0200

    Send password recovery

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/userModel.js";
+import { getResetPasswordTemplate } from "../utils/emailTemplates.js";
+import sendEmail from "../utils/sendEmail.js";
 import sendToken from "../utils/sendToken.js";
 import ErrorHandler from "../utils/errorHandler.js";
 // Register user => /api/v1/register
@@ -51,5 +53,52 @@ export const logoutUser = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
         message: "Logged Out",
     })
+})
+
+
+
+// Forgot password => /api/v1/password/forgot
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+
+
+    // Find user in the database  
+    const user = await User.findOne({ email: req.body.email })
+
+
+    if (!user) {
+        return next(new ErrorHandler('User not found with this email', 404))
+    }
+
+    // Get reset password token
+    const resetToken = user.getResetPasswordToken()
+
+    await user.save();
+
+    // Create reset password url 
+    const resetUrl = `${process.env.FRONTEND_URL}/api/v1/password/reset/${resetToken}`
+
+    const message = getResetPasswordTemplate(user?.name, resetUrl)
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: "ShopIT Password Recovery",
+            message,
+        });
+
+        res.status(200).json({
+            message: `Email sent to: ${user.email}`
+        })
+        return;
+
+    } catch (error) {
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpire = undefined
+
+        await user.save()
+        return next(new ErrorHandler(error?.message, 500))
+    }
+
+
 })
 
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
-import {registerUser, loginUser, logoutUser} from "../controllers/authController.js";
+import {registerUser, loginUser, logoutUser, forgotPassword} from "../controllers/authController.js";
 
 
+
+router.route("/password/forgot").post(forgotPassword)
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser)
