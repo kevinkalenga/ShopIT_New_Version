44b27845aabf7fc80dcc6f70b9c18fd0df7935e9
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-10 12:20:43 +0200

    Change and Update password

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,20 @@
 import express from "express";
 const router = express.Router();
 import {isAuthenticatedUser } from "../middlewares/auth.js"
-import {registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile} from "../controllers/authController.js";
+import {registerUser, loginUser, logoutUser, forgotPassword, resetPassword, getUserProfile, updatePassword} from "../controllers/authController.js";
 
 
-
+// pwd
 router.route("/password/forgot").post(forgotPassword)
 router.route("/password/reset/:token").put(resetPassword)
 
+// Auth
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
 
+// User Info
 router.route("/me").get(isAuthenticatedUser, getUserProfile)
+router.route("/password/update").put(isAuthenticatedUser, updatePassword)
 
 export default router
