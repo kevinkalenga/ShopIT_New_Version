4350cec99346a2d1d7120cb8d98b26c2ea37f67b
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-10 10:43:36 +0200

    Handle Duplicate key and jwt error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -7,7 +7,15 @@ export default (err, req, res, next) => {
         message: err?.message || "Internal Server Error",
     };
 
-     // Handle Invalid Mongoose ID Error 
+      // Handle Invalid Mongoose Duplicate Key Error
+
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered.`
+        error = new ErrorHandler(message, 400)
+    }
+    
+    
+    // Handle Invalid Mongoose ID Error 
 
     if (error.name === 'CastError') {
         const message = `Resource not found. Invalid: ${err?.path}`
