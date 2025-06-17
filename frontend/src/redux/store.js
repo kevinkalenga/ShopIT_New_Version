923bac93eb4e1d28557814d6c562ba9e07c8d12e
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-17 10:53:12 +0200

    Create redux toolkit store

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore} from "@reduxjs/toolkit";
+
+
+// create store which is going to be use in our entry point (index.js)
+
+export const store = configureStore({
+    reducer: {},
+    
+    
+})
