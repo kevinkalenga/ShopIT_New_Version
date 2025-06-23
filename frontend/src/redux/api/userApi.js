c7e88b715577f1e2289169808a60aa90775cba98
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-24 01:56:39 +0200

    Handle reset password

--- frontend/src/redux/api/userApi.js
+++ frontend/src/redux/api/userApi.js
@@ -67,6 +67,16 @@ export const userApi = createApi({
             },
             
         }),
+         resetPassword: builder.mutation({
+            query({token, body}) {
+                return {
+                    url: `/password/reset/${token}`,
+                    method: "PUT",
+                    body,
+                }
+            },
+            
+        })
         
     }),
 });
@@ -77,4 +87,5 @@ export const {
     useUploadAvatarMutation, 
     useUpdatePasswordMutation,
     useForgotPasswordMutation,
+    useResetPasswordMutation
 } = userApi;
