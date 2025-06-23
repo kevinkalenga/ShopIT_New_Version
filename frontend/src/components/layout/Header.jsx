5df8edc1bca3cf0a8d7b5b1b1b2565f3b541c2b3
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-23 11:08:26 +0200

    Show user in Header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,62 +1,78 @@
 
 import React from 'react'
 import Search from './Search'
+import { useSelector } from 'react-redux';
 import { useGetMeQuery } from '../../redux/api/userApi';
+import { Link} from 'react-router-dom';
 
  const Header = () => {
   
-  const { data } = useGetMeQuery()
-  console.log(data)
+    const { isLoading } = useGetMeQuery()
+   const { user } = useSelector(state => state.auth)
   
   return (
+    
      <nav className="navbar row">
-      <div className="col-12 col-md-3 ps-5">
-        <div className="navbar-brand">
-          <a href="/">
-            <img src="/images/shopit_logo.png" alt="ShopIT Logo" />
-          </a>
-        </div>
-      </div>
-      <div className="col-12 col-md-6 mt-2 mt-md-0">
-       <Search />
-      </div>
-      <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-        <a href="/cart" style={{textDecoration: "none"}}>
-          <span id="cart" className="ms-3"> Cart </span>
-          <span className="ms-1" id="cart_count">0</span>
-        </a>
+            <div className="col-12 col-md-3 ps-5">
+                <div className="navbar-brand">
+                    <Link to="/">
+                        <img src="/images/shopit_logo.png" alt="ShopIT Logo" />
+                    </Link>
+                </div>
+            </div>
+            <div className="col-12 col-md-6 mt-2 mt-md-0">
+                <Search />
+            </div>
+            <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                <a href="/cart" style={{ textDecoration: "none" }}>
+                    <span id="cart" className="ms-3"> Cart </span>
+                    <span className="ms-1" id="cart_count">
+                       0
+                    </span>
+                </a>
+                {
+                    user ? (
+                        <div className="ms-4 dropdown">
+                            <button
+                                className="btn dropdown-toggle text-white"
+                                type="button"
+                                id="dropDownMenuButton"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <figure className="avatar avatar-nav">
+                                    <img
+                                        src={user?.avatar ? user?.avatar.url : "/images/default_avatar.jpg"}
+                                        alt="User Avatar"
+                                        class="rounded-circle"
+                                    />
+                                </figure>
+                                <span>{user?.name}</span>
+                            </button>
+                            <div className="dropdown-menu w-100" aria-labelledby="dropDownMenuButton">
+                                <Link className="dropdown-item" to="/admin/dashboard"> Dashboard </Link>
 
-        <div className="ms-4 dropdown">
-          <button
-            className="btn dropdown-toggle text-white"
-            type="button"
-            id="dropDownMenuButton"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <figure className="avatar avatar-nav">
-              <img
-                src="../images/default_avatar.jpg"
-                alt="User Avatar"
-                className="rounded-circle"
-              />
-            </figure>
-            <span>User</span>
-          </button>
-          <div className="dropdown-menu w-100" aria-labelledby="dropDownMenuButton">
-            <a className="dropdown-item" href="/admin/dashboard"> Dashboard </a>
+                                <Link className="dropdown-item" to="/me/orders"> Orders </Link>
 
-            <a className="dropdown-item" href="/me/orders"> Orders </a>
+                                <Link className="dropdown-item" to="/me/profile"> Profile </Link>
 
-            <a className="dropdown-item" href="/me/profile"> Profile </a>
+                                <Link className="dropdown-item text-danger" to="/"> Logout </Link>
+                            </div>
+                        </div>
 
-            <a className="dropdown-item text-danger" href="/"> Logout </a>
-          </div>
-        </div>
+                    ) : (
+                        !isLoading && (
+                            <Link to="/login" className="btn ms-4" id="login_btn"> Login </Link>
+                        )
+                    )}
 
-        <a href="/login" className="btn ms-4" id="login_btn"> Login </a>
-      </div>
-    </nav>
+
+
+
+            </div>
+        </nav>
+    
+  
   )
 }
 
