015566e7bae6f79e879ceb043be42741c9497087
kevin kalenga <kevinkalenga10@gmail.com>
2025-06-25 00:11:39 +0200

    Handle quantity selection

--- frontend/src/components/product/ProductDetails.jsx
+++ frontend/src/components/product/ProductDetails.jsx
@@ -7,6 +7,7 @@ import renderStars from '../../utils/renderStars';
 import Loader from '../layout/Loader';
 
 const ProductDetails = () => {
+     const [quantity, setQuantity] = useState(1)
       const [activeImg, setActiveImg] = useState("")
       const params = useParams()
     const { data, isLoading, error, isError } = useGetProductDetailsQuery(params?.id)
@@ -22,10 +23,26 @@ const ProductDetails = () => {
         }
     }, [isError])
 
+    const increseQty = () => {
+       const count = document.querySelector(".count")
+       if(count.valueAsNumber >= product.stock) return;
+
+       const qty = count.valueAsNumber + 1 
+       setQuantity(qty)
+    }
+
+    const decreseQty = () => {
+         const count = document.querySelector(".count")
+       if(count.valueAsNumber <= 1) return;
+
+       const qty = count.valueAsNumber - 1 
+       setQuantity(qty)
+    }
     
     
     
-     if (isLoading) return <Loader />
+    
+    if (isLoading) return <Loader />
     
     return(
     <>
@@ -83,14 +100,14 @@ const ProductDetails = () => {
 
         <p id="product_price">${product?.price}</p>
         <div className="stockCounter d-inline">
-          <span className="btn btn-danger minus">-</span>
+          <span className="btn btn-danger minus" onClick={decreseQty}>-</span>
           <input
             type="number"
             className="form-control count d-inline"
-            value="1"
+            value={quantity}
             readonly
           />
-          <span className="btn btn-primary plus">+</span>
+          <span className="btn btn-primary plus" onClick={increseQty}>+</span>
         </div>
         <button
           type="button"
